bae72f50e7efbbcc3b025b29cb55dd1e3ca21a3c
Anjali Chouthani <chouthanianjali@gmail.com>
2025-03-14 10:08:46 +0530

    Updated

--- spark.java
+++ spark.java
@@ -2,7 +2,7 @@ public class spark{
     public static void main(String[] args) {
         System.out.println("hello");
     }
-    public void m1(){
+    public void payment(){
         System.out.println("modfied");
     }
 }
